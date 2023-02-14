78e76b19967e1c5c32d1851b37132bea3b15ac65
stuart540 <114927873+stuart540@users.noreply.github.com>
2023-02-14 15:24:09 +0000

    used moment to convert unix time into date

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,24 +1,33 @@
 var key = "8129706d5fbfcfb67a442f82bf57cab0"
 var cityName = "London"
 var apiURL =`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${key}&units=metric`
-var displayIcon = $('<div id="iconContainer"><img id="iconIMG" src="" alt="Weather icon"></div>');
 
 console.log(apiURL);
- 
-// call to open weather map
+
+// call to open weather map api
 $.ajax({
     url: apiURL,
     method: "GET"
 }).then(function (result) {
     console.log(result);
+    
+    var city = result.city.name
+    var unixTime = result.list[0].dt
+    var dateToday = moment.unix(unixTime).format(" DD/MM/YYYY")
+    console.log(city + dateToday);
+    
     var iconCode = result.list[0].weather[0].icon
     var weatherIcon = "http://openweathermap.org/img/w/" + iconCode + ".png";
+    var displayIcon = $('<div id="iconContainer"><img id="iconIMG" src="" alt="Weather icon"></div>');
     $("#today").append(displayIcon)
     $('#iconIMG').attr('src', weatherIcon);
+
     var temp = result.list[0].main.temp;
     console.log("Temp: " + temp + " °C");
+
     var wind = result.list[0].wind.speed;
     console.log("Wind: " + wind + " KPH");
+    
     var humidity = result.list[0].main.humidity;
     console.log("Humidity: " + humidity + "%");
 });
