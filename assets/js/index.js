a033a1d83291d7def557255a541936596a743689
stuart540 <114927873+stuart540@users.noreply.github.com>
2023-02-14 13:16:20 +0000

    result requested in metric

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,10 @@
 var key = "8129706d5fbfcfb67a442f82bf57cab0"
 var cityName = "London"
-var apiURL =`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${key}`
+var apiURL =`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${key}&units=metric`
 
 console.log(apiURL);
-    
+ 
+// call to open weather map
 $.ajax({
     url: apiURL,
     method: "GET"
