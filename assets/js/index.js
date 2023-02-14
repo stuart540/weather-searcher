f7315e46507de5a380e4aa2a442b190e03013d1e
stuart540 <114927873+stuart540@users.noreply.github.com>
2023-02-14 12:17:18 +0000

    linked css and js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,13 @@
 var key = "8129706d5fbfcfb67a442f82bf57cab0"
-var qURL =`https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}`
+var apiURL =`https://api.openweathermap.org/data/2.5/forecast?id=524901&appid=` + key
+
+console.log(apiURL);
+    
+// $.ajax({
+//     url: apiURL,
+//     method: "GET"
+// }).then(function (result) {
+//     console.log(result);
+    
+// });
+
